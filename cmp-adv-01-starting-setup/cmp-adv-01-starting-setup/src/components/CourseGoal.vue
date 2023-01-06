48f09941a52c9e965fb37b67851a299ffa6de10d
Adarsh <adarsh.vodnala@deltax.com>
2023-01-06 19:12:47 +0530

    okok

---
+++ cmp-adv-01-starting-setup/cmp-adv-01-starting-setup/src/components/CourseGoal.vue
@@ -0,0 +1,17 @@
+<template>
+    <ul>
+        <li v-for="goal in goals" :key="goal">
+            <slot :item="goal" test-prop="okok"></slot>
+        </li>
+    </ul>
+</template>
+
+<script>
+    export default{
+        data(){
+            return {
+                goals:['goal1','goal2']
+            }
+        }
+    }
+</script>
