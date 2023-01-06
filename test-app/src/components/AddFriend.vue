62536a05dff5d5cb4081b32c59775ea722a0b2af
Adarsh <adarsh.vodnala@deltax.com>
2023-01-06 13:55:20 +0530

    added components

---
+++ test-app/src/components/AddFriend.vue
@@ -0,0 +1,38 @@
+<template>
+    <form action="" @submit.prevent="submitData">
+        <div>
+            <label for="name">Name:</label>
+            <input type="text" id="name" v-model="name" required>
+        </div>
+        <div>
+            <label for="Phone">Phone:</label>
+            <input type="number" id="phone" v-model="phoneNumber" required>
+        </div>
+        <div>
+            <label for="email">Email:</label>
+            <input type="email" id="email" v-model="email" required>
+        </div>
+        <button type="submit">Submit</button>
+    </form>
+</template>
+
+<script>
+export default{
+    data(){
+        return{
+            name:'',
+            email:'',
+            phoneNumber:0
+        }
+    },
+    methods:{
+        submitData(){
+            this.$emit('add-data',this.name,this.email,this.phoneNumber);
+            this.name='';
+            this.email='';
+            this.phoneNumber='';
+        }
+    }
+
+};
+</script>
---
+++ test-app/src/components/FriendContact.vue
@@ -0,0 +1,40 @@
+<template>
+  <li>
+    <h2>{{ name }}</h2>
+    <button @click="toggleDetails">{{ detailsAreVisible?'Hide': 'Show'}} Details</button>&nbsp;
+    <button @click="$emit('delete-contact',id)">Delete</button>
+    <ul v-if="detailsAreVisible">
+      <li>
+        <strong>Phone:</strong>
+        {{ phone }}
+      </li>
+      <li>
+        <strong>Email:</strong>
+        {{ email }}
+      </li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  props:['id','name','email','phone'],
+  data() {
+    return {
+      detailsAreVisible: false,
+      friend: {
+        id: "manuel",
+        name: "Manuel Lorenz",
+        phone: "0123 45678 90",
+        email: "manuel@localhost.com",
+      },
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    },
+
+  }
+};
+</script>
